d51a7facb0f6c4e642066d6dc37170a479ea0dec
Dionis <dionisoliveira@gmail.com>
2020-02-11 22:49:50 -0300

    Add correct folder to project.

--- WebApiDapper/2 - Domain/Interface/Repository/IUserRepositoryEF.cs
+++
@@ -1,9 +0,0 @@
-﻿
-
-namespace DDDSample._2__Domain.Interface.Repository
-{
-    public interface IUserRepositoryEF : IRepository<DDDSample.Domain.User>
-    {
-        DDDSample.Domain.User BuscarDadosDoUsuario(string codigoUsuario);
-    }
-}
